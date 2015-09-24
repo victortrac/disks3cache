4b5f8937f96d0ed3bc61cad82836818341bd2c66
Victor Trac <victor@impossibleventures.com>
2015-09-24 15:46:13 -0500

    disk3cache first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Package disks3cache provides an implementation of httpcache.Cache that
+caches locally on disk using diskv and also in Amazon S3.
+
+See https://github.com/gregjones/httpcache
---
+++ disks3cache.go
@@ -0,0 +1,54 @@
+package disks3cache
+
+import (
+	"log"
+
+	"github.com/gregjones/httpcache/diskcache"
+	"github.com/peterbourgon/diskv"
+	"sourcegraph.com/sourcegraph/s3cache"
+)
+
+
+type Cache struct {
+	disk 	*diskcache.Cache
+	s3 	*s3cache.Cache
+}
+
+func (c *Cache) Get(key string) (resp []byte, ok bool) {
+	// Check disk first
+	resp, ok = c.disk.Get(key)
+	if ok == true {
+		log.Printf("Found %v in disk cache", key)
+		return resp, ok
+	}
+	resp, ok = c.s3.Get(key)
+	if ok == true {
+		log.Printf("Found %v in s3 cache", key)
+		return resp, ok
+	}
+	log.Printf("%v not found in cache", key)
+	return []byte{}, ok
+}
+
+func (c *Cache) Set(key string, resp []byte) {
+	log.Printf("Setting key %v", key)
+	go c.disk.Set(key, resp)
+	go c.s3.Set(key, resp)
+}
+
+func (c *Cache) Delete(key string) {
+	log.Printf("Deleting key %v", key)
+	go c.disk.Delete(key)
+	go c.s3.Delete(key)
+}
+
+func New(cacheDir string, cacheSize uint64, bucketURL string) *Cache {
+	dv := diskv.New(diskv.Options{
+		BasePath: 	cacheDir,
+		CacheSizeMax:	cacheSize * 1024 * 1024,
+	})
+	return &Cache{
+		disk:	diskcache.NewWithDiskv(dv),
+		s3:	s3cache.New(bucketURL),
+	}
+}
